7e835804d7d249a3535db9c5c5c24116cc1740aa
Edward Thang <RandomClown@gmail.com>
2012-12-03 02:02:03 -0800

    Equation Generator [Lvl 10]
    - Completed the eq. gen.
    - Renamed the read me to be detected by Git

--- Libs/Equation Generator.js
+++ Libs/Equation Generator.js
@@ -1,20 +1,82 @@
-﻿maxLVL_generateEQ = 5;
+﻿maxLVL_generateEQ = 10;
 
-function generateEQ(level) {
-	var MAX = 16;
+function generateEQ(level, MAX_NUM) {
+	if (MAX_NUM == undefined) MAX_NUM = 16;
 
-	function struct() { this.EQ = ""; this.answer = 0; };
-	// Gets the common divisor of 2 numbers
 
-	ans = new struct();
-	function assemble(b, x, a, c1, c2) {
-		// Formats a positive & negative number differently.
+
+	//========================
+	//			Structs
+	function Answer() { this.EQ = ""; this.answer = 0; };
+	function Equation() {
+		this.aDen;
+		this.xDen;
+
+		//	For any expression in the form of:
+		//		ax + c = b
+		this.a;
+		this.x;
+		this.c1;
+		this.c2;
+		this.b;
+
+
+
+		this.init = function (num) {
+			if (num == undefined) num = 0;
+
+			this.aDen = 0;
+			this.xDen = 0;
+
+			this.a = 1;
+			this.c1 = 0;
+			this.c2 = 0;
+			this.x = this.b = num;
+		}
+
+		this.translate = function (c1, c2) {
+			if (c1 != undefined) this.b += (this.c1 = c1);
+			if (c2 != undefined) this.b += (this.c2 = c2);
+		}
+		this.mul = function (num) {
+			this.b = (this.b - this.c1 - this.c2) * num + this.c1 + this.c2;
+			this.a = (this.b - this.c1 - this.c2) / this.x;
+		}
+		this.seedDiv = function (num) {
+			this.init(roll(MAX_NUM));
+
+			if (roll(10) % 2 == 0) { // Choose a/x or x/a
+				//document.write(eq.a + '|' + eq.x + '|' + eq.c1 + '|' + eq.b + '<br>');
+				eq.b = num;
+				eq.a = eq.x + 1; // Make non zero
+				eq.x = eq.a * eq.b;
+				eq.a = '/' + eq.a;
+			} else {
+				eq.b = num + 1; // Make non zero
+				eq.x = eq.x + 1;
+				eq.a = eq.x * eq.b;
+				eq.x = '/' + eq.x;
+			}
+		}
+
+		// 
+		this.init(roll(MAX_NUM));
+	};
+	//			Structs
+	//========================
+
+
+
+	//========================
+	function assemble(eq) {
+		// Formats eq.a positive & negative number differently.
 		function pm(n) {
 			return '' + (n < 0 ? '(' + n + ')' : n);
 		}
-		if (a == undefined) a = 1;
-		if (c1 == undefined) c1 = 0;
-		if (c2 == undefined) c2 = 0;
+
+		if (eq.a == undefined) eq.a = 1;
+		if (eq.c1 == undefined) eq.c1 = 0;
+		if (eq.c2 == undefined) eq.c2 = 0;
 
 		var lhs = new Array();
 
@@ -23,117 +85,103 @@ function generateEQ(level) {
 		o2 = roll(1); if (o1 <= o2) o2++;
 		o3 = (o1 == 0 || o2 == 0 ? (o1 == 1 || o2 == 1 ? 2 : 1) : 0);
 
-		var xFrac = 0, isFrac = (typeof a == 'string') || (typeof x == 'string');
+		var xFrac = 0, isFrac = (typeof eq.a == 'string') || (typeof eq.x == 'string');
 		if (isFrac) {
-			if (typeof a == 'string') {
-				a = Number(a.substring(1, a.length));
+			if (typeof eq.a == 'string') {
+				eq.a = Number(eq.a.substring(1, eq.a.length));
 				xFrac = 0;
 			}
-			if (typeof x == 'string') {
-				x = Number(x.substring(1, x.length));
+			if (typeof eq.x == 'string') {
+				eq.x = Number(eq.x.substring(1, eq.x.length));
 				xFrac = 1;
 			}
 		}
 
-		if (a != 1) {
+		if (eq.a != 1) {
 			if (isFrac) {
-				if (xFrac) lhs[o1] = pm(a) + '÷x';
-				else lhs[o1] = 'x÷' + pm(a);
+				if (xFrac) lhs[o1] = pm(eq.a) + '÷x';
+				else lhs[o1] = 'x÷' + pm(eq.a);
 			} else {
-				lhs[o1] = pm(a) + '•x';
+				lhs[o1] = pm(eq.a) + '•x';
 			}
 		} else {
 			lhs[o1] = 'x';
 		}
 
-		lhs[o2] = (c1 == 0) ? '' : '' + pm(c1);
-		lhs[o3] = (c2 == 0) ? '' : '' + pm(c2);
+		lhs[o2] = (eq.c1 == 0) ? '' : '' + pm(eq.c1);
+		lhs[o3] = (eq.c2 == 0) ? '' : '' + pm(eq.c2);
 
 
 
-		ans.answer = x;
+		ans.answer = eq.x;
 		ans.EQ = lhs[0];
 		if (lhs[1] != '') ans.EQ += (ans.EQ == '' ? '' : ' + ') + lhs[1];
 		if (lhs[2] != '') ans.EQ += (ans.EQ == '' ? '' : ' + ') + lhs[2];
-		ans.EQ += ' = ' + b;
+		ans.EQ += ' = ' + eq.b;
 	}
+	//========================
+	
 
 
-	//		For any expression in the form of:
-	// ax+c=n
-	var factor, temp = 0;
-	var a, x, c, b;
-	a = 1; x = c = b = 0;
-
-
-
-	if (1 <= level) { // Level 1+
-		x = roll(MAX);
-		if (x < 2) x = roll(MAX); // Reduce chances of getting 0 & 1
-		b = x;
-		assemble(b, x);
-	}
+	//========================
+	//			Level Actions
+	var ans = new Answer();
+	var eq = new Equation(); // Level 1 is handled on creation
 
 	if (2 <= level) { // Level 2+
-		temp = roll(x / 3 * 2); // Roll a C
-		if (temp < 2) temp = roll(x / 3 * 2); // Reduce chances of getting 0 & 1
+		var temp = roll(MAX_NUM / 3 * 2); // Roll a C
+		if (temp < 2) temp = roll(MAX_NUM / 3 * 2); // Reduce chances of getting 0 & 1
 
 		switch (level) {
-			case 2:
-				c = temp;
-				b += temp;
+			case 2: // x + c = b
+				eq.translate(roll(MAX_NUM));
 				break;
 
-			case 3:
-				temp *= (Math.pow(-1, roll(10)))
-				c = temp;
-				b += temp;
+			case 3: // x - c = b
+				eq.translate(-roll(MAX_NUM));
 				break;
 
 			case 4: // ax = b
-				while (temp < 1) temp++;
-				a = temp;
-				b *= temp;
+				while (temp == 0) temp = roll(MAX_NUM);
+				eq.mul(temp);
 				break;
 
-			case 5: // ax = b   =>   x = ab
-				if (roll(10) % 2 == 0) { // Choose a/x or x/a
-					b = temp;
-					a = x + 1; // Make non zero
-					x = a * b;
-					a = '/' + a;
-				} else {
-					b = temp + 1; // Make non zero
-					x = x + 1;
-					a = x * b;
-					x = '/' + x;
-				}
-
+			case 5: // x/a = b   =>   x = ab
+				while (temp == 0) temp = roll(MAX_NUM);
+				eq.seedDiv(temp);
 				break;
 		}
-
-		assemble(b, x, a, c);
 	}
 
 	if (6 <= level) { // Level 6+
+		var temp1 = roll(MAX_NUM / 3 * 2) * (Math.pow(-1, roll(10))); // Roll 2 C's
+		var temp2 = roll(MAX_NUM / 3 * 2) * (Math.pow(-1, roll(10)));
+
+		switch (level) {
+			case 6: // x +- c1 +- c2 = b
+				break;
+
+			case 7: // ax +- c1... = b
+				eq.mul(roll(MAX_NUM / 3 * 2));
+				break;
+
+			case 8: // a/x +- c1... = b
+			case 9: // x/a = b
+				eq.seedDiv(roll(MAX_NUM / 3 * 2));
+				break;
+
+			case 10: // x/*a +- c1... = b
+				if (roll(10) % 2 == 0) eq.seedDiv(roll(MAX_NUM / 3 * 2));
+				else eq.mul(roll(MAX_NUM / 3 * 2));
+				break;
+		}
+
+		eq.translate(temp1, temp2);
 	}
 
-
+	assemble(eq);
+	//			Level Actions
+	//========================
 
 	return ans;
 }
-
-/*		Reference:
-	level 1 should be in the form:  x = b
-
-	level 2 should be in the form:  x + c = b
-	level 3 should be in the form:  x - c = b
-	level 4 should be in the form:  x * c = b
-	level 5 should be in the form:  x / c = b
-
-	level 6 should be in the form:  x +- c1 +- c2 = b
-	level 7 should be in the form:  ax +- c... = b
-	level 8 should be in the form:  a/x +- c... = b
-	level 9 should be in the form:  x/a = b
-	level 10 should be in the form: x/*a +- c... = b
-*/
