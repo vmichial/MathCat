5723ed352df69ca74ab1172ca4db6866901d6611
Edward Thang <RandomClown@gmail.com>
2012-12-11 17:17:17 -0800

    OCD Formatting
    - Formatted white space according to my OCD

--- Libs/Deck.js
+++ Libs/Deck.js
@@ -1,125 +1,125 @@
 ﻿function cardV(num) {
 	//if (num < 0 || 52 <= num) throw ('Index out of bound of standard deck [0-51].  Attempted to call: ' + 'cardV(' + num + ')');
-	return Math.floor(num / 4);
+	return Math.floor(num/4);
 }
 function cardS(num) {
-	return num % 4;
+	return num%4;
 }
 function cardFile(num) {
 	var suitName;
-	switch (cardS(num)) {
-		case 0: suitName = 'brown';
+	switch(cardS(num)) {
+		case 0: suitName='brown';
 			break;
-		case 1: suitName = 'green';
+		case 1: suitName='green';
 			break;
-		case 2: suitName = 'blue';
+		case 2: suitName='blue';
 			break;
-		case 3: suitName = 'purple';
+		case 3: suitName='purple';
 			break;
 	}
-	return 'card' + (cardV(num) + 1) + suitName + '.png';
+	return 'card'+(cardV(num)+1)+suitName+'.png';
 }
 
-var Deck = function (deckSize) {
-	if (deckSize == undefined) deckSize = 52;
+var Deck=function(deckSize) {
+	if(deckSize==undefined) deckSize=52;
 
 
 
 	// --====[ Variables ]====--
-	this.DECKSIZE = deckSize;
+	this.DECKSIZE=deckSize;
 	this.cardCount;
 
-	this.selected = new Array(); // An array of size [deck size / (byte size(int))]
-	this.old_selected = new Array();
+	this.selected=new Array(); // An array of size [deck size / (byte size(int))]
+	this.old_selected=new Array();
 
-	this.cards = new Array();
-	this.old_cards = new Array();
+	this.cards=new Array();
+	this.old_cards=new Array();
 
 
 
 	// --====[ Methods ]====--
-	this.init = function () {
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
-			this.selected[i] = this.old_selected[i] = 0;
-		for (var i = 0, e = this.DECKSIZE; i < e; i++)
-			this.cards[i] = this.old_cards[i] = i;
-		this.cardCount = this.DECKSIZE;
+	this.init=function() {
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++)
+			this.selected[i]=this.old_selected[i]=0;
+		for(var i=0, e=this.DECKSIZE; i<e; i++)
+			this.cards[i]=this.old_cards[i]=i;
+		this.cardCount=this.DECKSIZE;
 	}
 
-	this.isSelectedV = function (num) {
-		return 0 != (this.selected[Math.floor(num / 32)] & (1 << (num % 32)));
+	this.isSelectedV=function(num) {
+		return 0!=(this.selected[Math.floor(num/32)]&(1<<(num%32)));
 	}
-	this.isSelectedD = function (num) {
-		num = this.cards[num];
+	this.isSelectedD=function(num) {
+		num=this.cards[num];
 		return this.isSelected(num);
 	}
 
-	this.selectV = function (num) {
-		this.selected[Math.floor(num / 32)] |= (1 << (num % 32));
+	this.selectV=function(num) {
+		this.selected[Math.floor(num/32)]|=(1<<(num%32));
 	}
-	this.deselectV = function (num) {
-		this.selected[Math.floor(num / 32)] &= ~(1 << (num % 32));
+	this.deselectV=function(num) {
+		this.selected[Math.floor(num/32)]&=~(1<<(num%32));
 	}
-	this.selectD = function (num) {
-		num = this.cards[num];
+	this.selectD=function(num) {
+		num=this.cards[num];
 		this.selectV(num);
 	}
-	this.deselectD = function (num) {
-		num = this.cards[num];
+	this.deselectD=function(num) {
+		num=this.cards[num];
 		this.deselectV(num);
 	}
 
-	this.selectAll = function () {
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
-			this.selected[i] = 0xffffffff;
+	this.selectAll=function() {
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++)
+			this.selected[i]=0xffffffff;
 	}
-	this.deselectAll = function () {
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
-			this.selected[i] = 0;
-	}
-
-	this.selectInverse = function () {
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
-			this.selected[i] = ~this.selected[i];
+	this.deselectAll=function() {
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++)
+			this.selected[i]=0;
 	}
 
-	this.stateSave = function () {
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
-			this.old_selected[i] = this.selected[i];
-	}
-	this.stateLoad = function () {
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
-			this.selected[i] = this.old_selected[i];
+	this.selectInverse=function() {
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++)
+			this.selected[i]=~this.selected[i];
 	}
 
-	this.setCards = function (list, listSize) {
-		this.DECKSIZE = listSize;
+	this.stateSave=function() {
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++)
+			this.old_selected[i]=this.selected[i];
+	}
+	this.stateLoad=function() {
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++)
+			this.selected[i]=this.old_selected[i];
+	}
+
+	this.setCards=function(list, listSize) {
+		this.DECKSIZE=listSize;
 		this.init();
-		for (var i = 0; i < listSize; i++) this.cards[i] = list[i];
+		for(var i=0; i<listSize; i++) this.cards[i]=list[i];
 	}
 
-	this.shuffle = function () {
-		var r, new_cards = new Array();
-		for (var i = this.DECKSIZE - 1; 0 <= i; i--) {
-			r = roll(i);
-			new_cards[i] = this.cards[r];
-			this.cards[r] = this.cards[i];
+	this.shuffle=function() {
+		var r, new_cards=new Array();
+		for(var i=this.DECKSIZE-1; 0<=i; i--) {
+			r=roll(i);
+			new_cards[i]=this.cards[r];
+			this.cards[r]=this.cards[i];
 		}
-		for (var i = this.DECKSIZE - 1; 0 <= i; i--) {
-			r = roll(i);
-			this.cards[i] = new_cards[r];
-			new_cards[r] = new_cards[i];
+		for(var i=this.DECKSIZE-1; 0<=i; i--) {
+			r=roll(i);
+			this.cards[i]=new_cards[r];
+			new_cards[r]=new_cards[i];
 		}
 	}
 
-	this.listSelected = function (list) {
-		var listSize = 0;
-		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++) {
-			var s, selection = this.selected[i];
-			while (selection != 0) {
-				s = selection & -selection
-				selection &= ~s;
-				list[listSize++] = (log2(s) + i * 32);
+	this.listSelected=function(list) {
+		var listSize=0;
+		for(var i=0, e=Math.floor(this.DECKSIZE/32)+1 ; i<e; i++) {
+			var s, selection=this.selected[i];
+			while(selection!=0) {
+				s=selection&-selection
+				selection&=~s;
+				list[listSize++]=(log2(s)+i*32);
 			}
 		}
 		return listSize;
@@ -137,33 +137,33 @@ var Deck = function (deckSize) {
 
 
 	// --====[ Common Case Errors ]====--
-	this.isSelectedv = function (num) { throw (throw_case('Deck.isSelectedV')); }
-	this.isSelectedd = function (num) { throw (throw_case('Deck.isSelectedD')); }
-	this.isselectedV = function (num) { throw (throw_case('Deck.isSelectedV')); }
-	this.isselectedD = function (num) { throw (throw_case('Deck.isSelectedD')); }
-	this.isselectedv = function (num) { throw (throw_case('Deck.isSelectedV')); }
-	this.isselectedd = function (num) { throw (throw_case('Deck.isSelectedD')); }
+	this.isSelectedv=function(num) { throw (throw_case('Deck.isSelectedV')); }
+	this.isSelectedd=function(num) { throw (throw_case('Deck.isSelectedD')); }
+	this.isselectedV=function(num) { throw (throw_case('Deck.isSelectedV')); }
+	this.isselectedD=function(num) { throw (throw_case('Deck.isSelectedD')); }
+	this.isselectedv=function(num) { throw (throw_case('Deck.isSelectedV')); }
+	this.isselectedd=function(num) { throw (throw_case('Deck.isSelectedD')); }
 
-	this.SelectV = function (num) { throw (throw_case('Deck.selectV')); }
-	this.DeselectV = function (num) { throw (throw_case('Deck.deselectV')); }
-	this.SelectD = function (num) { throw (throw_case('Deck.selectD')); }
-	this.DeselectD = function (num) { throw (throw_case('Deck.seselectD')); }
+	this.SelectV=function(num) { throw (throw_case('Deck.selectV')); }
+	this.DeselectV=function(num) { throw (throw_case('Deck.deselectV')); }
+	this.SelectD=function(num) { throw (throw_case('Deck.selectD')); }
+	this.DeselectD=function(num) { throw (throw_case('Deck.seselectD')); }
 
-	this.Selectv = function (num) { throw (throw_case('Deck.selectV')); }
-	this.Deselectv = function (num) { throw (throw_case('Deck.deselectV')); }
-	this.Selectd = function (num) { throw (throw_case('Deck.selectD')); }
-	this.Deselectd = function (num) { throw (throw_case('Deck.seselectD')); }
+	this.Selectv=function(num) { throw (throw_case('Deck.selectV')); }
+	this.Deselectv=function(num) { throw (throw_case('Deck.deselectV')); }
+	this.Selectd=function(num) { throw (throw_case('Deck.selectD')); }
+	this.Deselectd=function(num) { throw (throw_case('Deck.seselectD')); }
 
-	this.selectv = function (num) { throw (throw_case('Deck.selectV')); }
-	this.deselectv = function (num) { throw (throw_case('Deck.deselectV')); }
-	this.selectd = function (num) { throw (throw_case('Deck.selectD')); }
-	this.deselectd = function (num) { throw (throw_case('Deck.seselectD')); }
+	this.selectv=function(num) { throw (throw_case('Deck.selectV')); }
+	this.deselectv=function(num) { throw (throw_case('Deck.deselectV')); }
+	this.selectd=function(num) { throw (throw_case('Deck.selectD')); }
+	this.deselectd=function(num) { throw (throw_case('Deck.seselectD')); }
 
-	this.selectall = function (num) { throw (throw_case('Deck.selectAll')); }
-	this.deselectall = function (num) { throw (throw_case('Deck.deselectAll')); }
+	this.selectall=function(num) { throw (throw_case('Deck.selectAll')); }
+	this.deselectall=function(num) { throw (throw_case('Deck.deselectAll')); }
 
-	this.SelectAll = function (num) { throw (throw_case('Deck.selectAll')); }
-	this.DeselectAll = function (num) { throw (throw_case('Deck.deselectAll')); }
+	this.SelectAll=function(num) { throw (throw_case('Deck.selectAll')); }
+	this.DeselectAll=function(num) { throw (throw_case('Deck.deselectAll')); }
 }
 
-function throw_case(correct) { return 'Wrong casing.  Correct version of this call: ' + correct + '(...)'; }
+function throw_case(correct) { return 'Wrong casing.  Correct version of this call: '+correct+'(...)'; }
--- Libs/Equation Generator.js
+++ Libs/Equation Generator.js
@@ -1,13 +1,13 @@
-﻿maxLVL_generateEQ = 10;
+﻿maxLVL_generateEQ=10;
 
 function generateEQ(level, MAX_NUM) {
-	if (MAX_NUM == undefined) MAX_NUM = 16;
+	if(MAX_NUM==undefined) MAX_NUM=16;
 
 
 
 	//========================
 	//			Structs
-	function Answer() { this.EQ = ""; this.answer = 0; };
+	function Answer() { this.EQ=""; this.answer=0; };
 	function Equation() {
 		this.aDen;
 		this.xDen;
@@ -22,40 +22,40 @@ function generateEQ(level, MAX_NUM) {
 
 
 
-		this.init = function (num) {
-			if (num == undefined) num = 0;
+		this.init=function(num) {
+			if(num==undefined) num=0;
 
-			this.aDen = 0;
-			this.xDen = 0;
+			this.aDen=0;
+			this.xDen=0;
 
-			this.a = 1;
-			this.c1 = 0;
-			this.c2 = 0;
-			this.x = this.b = num;
+			this.a=1;
+			this.c1=0;
+			this.c2=0;
+			this.x=this.b=num;
 		}
 
-		this.translate = function (c1, c2) {
-			if (c1 != undefined) this.b += (this.c1 = c1);
-			if (c2 != undefined) this.b += (this.c2 = c2);
+		this.translate=function(c1, c2) {
+			if(c1!=undefined) this.b+=(this.c1=c1);
+			if(c2!=undefined) this.b+=(this.c2=c2);
 		}
-		this.mul = function (num) {
-			this.b = (this.b - this.c1 - this.c2) * num + this.c1 + this.c2;
-			this.a = (this.b - this.c1 - this.c2) / this.x;
+		this.mul=function(num) {
+			this.b=(this.b-this.c1-this.c2)*num+this.c1+this.c2;
+			this.a=(this.b-this.c1-this.c2)/this.x;
 		}
-		this.seedDiv = function (num) {
+		this.seedDiv=function(num) {
 			this.init(roll(MAX_NUM));
 
-			if (roll(10) % 2 == 0) { // Choose a/x or x/a
+			if(roll(10)%2==0) { // Choose a/x or x/a
 				//document.write(eq.a + '|' + eq.x + '|' + eq.c1 + '|' + eq.b + '<br>');
-				eq.b = num;
-				eq.a = eq.x + 1; // Make non zero
-				eq.x = eq.a * eq.b;
-				eq.a = '/' + eq.a;
+				eq.b=num;
+				eq.a=eq.x+1; // Make non zero
+				eq.x=eq.a*eq.b;
+				eq.a='/'+eq.a;
 			} else {
-				eq.b = num + 1; // Make non zero
-				eq.x = eq.x + 1;
-				eq.a = eq.x * eq.b;
-				eq.x = '/' + eq.x;
+				eq.b=num+1; // Make non zero
+				eq.x=eq.x+1;
+				eq.a=eq.x*eq.b;
+				eq.x='/'+eq.x;
 			}
 		}
 
@@ -71,53 +71,53 @@ function generateEQ(level, MAX_NUM) {
 	function assemble(eq) {
 		// Formats eq.a positive & negative number differently.
 		function pm(n) {
-			return '' + (n < 0 ? '(' + n + ')' : n);
+			return ''+(n<0?'('+n+')':n);
 		}
 
-		if (eq.a == undefined) eq.a = 1;
-		if (eq.c1 == undefined) eq.c1 = 0;
-		if (eq.c2 == undefined) eq.c2 = 0;
+		if(eq.a==undefined) eq.a=1;
+		if(eq.c1==undefined) eq.c1=0;
+		if(eq.c2==undefined) eq.c2=0;
 
-		var lhs = new Array();
+		var lhs=new Array();
 
 		var o1, o2, o3;
-		o1 = roll(2);
-		o2 = roll(1); if (o1 <= o2) o2++;
-		o3 = (o1 == 0 || o2 == 0 ? (o1 == 1 || o2 == 1 ? 2 : 1) : 0);
+		o1=roll(2);
+		o2=roll(1); if(o1<=o2) o2++;
+		o3=(o1==0||o2==0?(o1==1||o2==1?2:1):0);
 
-		var xFrac = 0, isFrac = (typeof eq.a == 'string') || (typeof eq.x == 'string');
-		if (isFrac) {
-			if (typeof eq.a == 'string') {
-				eq.a = Number(eq.a.substring(1, eq.a.length));
-				xFrac = 0;
+		var xFrac=0, isFrac=(typeof eq.a=='string')||(typeof eq.x=='string');
+		if(isFrac) {
+			if(typeof eq.a=='string') {
+				eq.a=Number(eq.a.substring(1, eq.a.length));
+				xFrac=0;
 			}
-			if (typeof eq.x == 'string') {
-				eq.x = Number(eq.x.substring(1, eq.x.length));
-				xFrac = 1;
+			if(typeof eq.x=='string') {
+				eq.x=Number(eq.x.substring(1, eq.x.length));
+				xFrac=1;
 			}
 		}
 
-		if (eq.a != 1) {
-			if (isFrac) {
-				if (xFrac) lhs[o1] = pm(eq.a) + '÷x';
-				else lhs[o1] = 'x÷' + pm(eq.a);
+		if(eq.a!=1) {
+			if(isFrac) {
+				if(xFrac) lhs[o1]=pm(eq.a)+'÷x';
+				else lhs[o1]='x÷'+pm(eq.a);
 			} else {
-				lhs[o1] = pm(eq.a) + 'x';
+				lhs[o1]=pm(eq.a)+'x';
 			}
 		} else {
-			lhs[o1] = 'x';
+			lhs[o1]='x';
 		}
 
-		lhs[o2] = (eq.c1 == 0) ? '' : '' + pm(eq.c1);
-		lhs[o3] = (eq.c2 == 0) ? '' : '' + pm(eq.c2);
+		lhs[o2]=(eq.c1==0)?'':''+pm(eq.c1);
+		lhs[o3]=(eq.c2==0)?'':''+pm(eq.c2);
 
 
 
-		ans.answer = eq.x;
-		ans.EQ = lhs[0];
-		if (lhs[1] != '') ans.EQ += (ans.EQ == '' ? '' : ' + ') + lhs[1];
-		if (lhs[2] != '') ans.EQ += (ans.EQ == '' ? '' : ' + ') + lhs[2];
-		ans.EQ += ' = ' + eq.b;
+		ans.answer=eq.x;
+		ans.EQ=lhs[0];
+		if(lhs[1]!='') ans.EQ+=(ans.EQ==''?'':' + ')+lhs[1];
+		if(lhs[2]!='') ans.EQ+=(ans.EQ==''?'':' + ')+lhs[2];
+		ans.EQ+=' = '+eq.b;
 	}
 	//========================
 
@@ -125,14 +125,14 @@ function generateEQ(level, MAX_NUM) {
 
 	//========================
 	//			Level Actions
-	var ans = new Answer();
-	var eq = new Equation(); // Level 1 is handled on creation
+	var ans=new Answer();
+	var eq=new Equation(); // Level 1 is handled on creation
 
-	if (2 <= level) { // Level 2+
-		var temp = roll(MAX_NUM / 3 * 2); // Roll a C
-		if (temp < 2) temp = roll(MAX_NUM / 3 * 2); // Reduce chances of getting 0 & 1
+	if(2<=level) { // Level 2+
+		var temp=roll(MAX_NUM/3*2); // Roll a C
+		if(temp<2) temp=roll(MAX_NUM/3*2); // Reduce chances of getting 0 & 1
 
-		switch (level) {
+		switch(level) {
 			case 2: // x + c = b
 				eq.translate(roll(MAX_NUM));
 				break;
@@ -142,42 +142,42 @@ function generateEQ(level, MAX_NUM) {
 				break;
 
 			case 4: // ax = b
-				while (eq.b == 0) eq.init(roll(MAX_NUM));
-				while (temp == 0) temp = roll(MAX_NUM);
+				while(eq.b==0) eq.init(roll(MAX_NUM));
+				while(temp==0) temp=roll(MAX_NUM);
 				eq.mul(temp);
 				break;
 
 			case 5: // x/a = b   =>   x = ab
-				while (temp == 0) temp = roll(MAX_NUM);
+				while(temp==0) temp=roll(MAX_NUM);
 				eq.seedDiv(temp);
 				break;
 		}
 	}
 
-	if (6 <= level) { // Level 6+
-		var temp1 = roll(MAX_NUM / 3 * 2) * (Math.pow(-1, roll(10))); // Roll 2 C's
-		var temp2 = roll(MAX_NUM / 3 * 2) * (Math.pow(-1, roll(10)));
+	if(6<=level) { // Level 6+
+		var temp1=roll(MAX_NUM/3*2)*(Math.pow(-1, roll(10))); // Roll 2 C's
+		var temp2=roll(MAX_NUM/3*2)*(Math.pow(-1, roll(10)));
 
-		switch (level) {
+		switch(level) {
 			case 6: // x +- c1 +- c2 = b
 				break;
 
 			case 7: // ax +- c1... = b
-				while (eq.b == 0) eq.init(roll(MAX_NUM));
-				while (temp == 0) temp = roll(MAX_NUM);
-				eq.mul(roll(MAX_NUM / 3 * 2));
+				while(eq.b==0) eq.init(roll(MAX_NUM));
+				while(temp==0) temp=roll(MAX_NUM);
+				eq.mul(roll(MAX_NUM/3*2));
 				break;
 
 			case 8: // a/x +- c1... = b
 			case 9: // x/a = b
-				eq.seedDiv(roll(MAX_NUM / 3 * 2));
+				eq.seedDiv(roll(MAX_NUM/3*2));
 				break;
 
 			case 10: // x/*a +- c1... = b
-				if (roll(10) % 2 == 0) eq.seedDiv(roll(MAX_NUM / 3 * 2));
+				if(roll(10)%2==0) eq.seedDiv(roll(MAX_NUM/3*2));
 				else {
-					while (eq.b == 0) eq.init(roll(MAX_NUM));
-					eq.mul(roll(MAX_NUM / 3 * 2));
+					while(eq.b==0) eq.init(roll(MAX_NUM));
+					eq.mul(roll(MAX_NUM/3*2));
 				}
 				break;
 		}
