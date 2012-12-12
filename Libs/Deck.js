6e4b237b3c81b2232831c60bff69d4cc6fed1b86
Edward Thang <RandomClown@gmail.com>
2012-12-11 16:11:16 -0800

    Deck Class
    Updated class Deck:
    - Added selection methods for card values or deck position
    - Added common misspellings to help catch bad casing

--- Libs/Deck.js
+++ Libs/Deck.js
@@ -1,5 +1,5 @@
 ﻿function cardV(num) {
-	if (num < 0 || 52 <= num) throw ('Index out of bound of deck [0-51].  Attempted to call: ' + 'cardV(' + num + ')');
+	//if (num < 0 || 52 <= num) throw ('Index out of bound of standard deck [0-51].  Attempted to call: ' + 'cardV(' + num + ')');
 	return Math.floor(num / 4);
 }
 function cardS(num) {
@@ -24,8 +24,10 @@ var Deck = function (deckSize) {
 	if (deckSize == undefined) deckSize = 52;
 
 
+
 	// --====[ Variables ]====--
 	this.DECKSIZE = deckSize;
+	this.cardCount;
 
 	this.selected = new Array(); // An array of size [deck size / (byte size(int))]
 	this.old_selected = new Array();
@@ -34,24 +36,39 @@ var Deck = function (deckSize) {
 	this.old_cards = new Array();
 
 
+
 	// --====[ Methods ]====--
 	this.init = function () {
 		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
 			this.selected[i] = this.old_selected[i] = 0;
 		for (var i = 0, e = this.DECKSIZE; i < e; i++)
 			this.cards[i] = this.old_cards[i] = i;
+		this.cardCount = this.DECKSIZE;
 	}
 
-	this.isSelected = function (num) {
+	this.isSelectedV = function (num) {
 		return 0 != (this.selected[Math.floor(num / 32)] & (1 << (num % 32)));
 	}
+	this.isSelectedD = function (num) {
+		num = this.cards[num];
+		return this.isSelected(num);
+	}
 
-	this.select = function (num) {
+	this.selectV = function (num) {
 		this.selected[Math.floor(num / 32)] |= (1 << (num % 32));
 	}
-	this.deselect = function (num) {
+	this.deselectV = function (num) {
 		this.selected[Math.floor(num / 32)] &= ~(1 << (num % 32));
 	}
+	this.selectD = function (num) {
+		num = this.cards[num];
+		this.selectV(num);
+	}
+	this.deselectD = function (num) {
+		num = this.cards[num];
+		this.deselectV(num);
+	}
+
 	this.selectAll = function () {
 		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++)
 			this.selected[i] = 0xffffffff;
@@ -75,7 +92,7 @@ var Deck = function (deckSize) {
 			this.selected[i] = this.old_selected[i];
 	}
 
-	this.setCards=function(list, listSize){
+	this.setCards = function (list, listSize) {
 		this.DECKSIZE = listSize;
 		this.init();
 		for (var i = 0; i < listSize; i++) this.cards[i] = list[i];
@@ -99,7 +116,7 @@ var Deck = function (deckSize) {
 		var listSize = 0;
 		for (var i = 0, e = Math.floor(this.DECKSIZE / 32) + 1 ; i < e; i++) {
 			var s, selection = this.selected[i];
-			while (0 != selection) {
+			while (selection != 0) {
 				s = selection & -selection
 				selection &= ~s;
 				list[listSize++] = (log2(s) + i * 32);
@@ -108,7 +125,45 @@ var Deck = function (deckSize) {
 		return listSize;
 	}
 
+	// add
+	// del
+	// size
+
+
 
 	// --====[ Constructor ]====--
 	this.init();
+
+
+
+	// --====[ Common Case Errors ]====--
+	this.isSelectedv = function (num) { throw (throw_case('Deck.isSelectedV')); }
+	this.isSelectedd = function (num) { throw (throw_case('Deck.isSelectedD')); }
+	this.isselectedV = function (num) { throw (throw_case('Deck.isSelectedV')); }
+	this.isselectedD = function (num) { throw (throw_case('Deck.isSelectedD')); }
+	this.isselectedv = function (num) { throw (throw_case('Deck.isSelectedV')); }
+	this.isselectedd = function (num) { throw (throw_case('Deck.isSelectedD')); }
+
+	this.SelectV = function (num) { throw (throw_case('Deck.selectV')); }
+	this.DeselectV = function (num) { throw (throw_case('Deck.deselectV')); }
+	this.SelectD = function (num) { throw (throw_case('Deck.selectD')); }
+	this.DeselectD = function (num) { throw (throw_case('Deck.seselectD')); }
+
+	this.Selectv = function (num) { throw (throw_case('Deck.selectV')); }
+	this.Deselectv = function (num) { throw (throw_case('Deck.deselectV')); }
+	this.Selectd = function (num) { throw (throw_case('Deck.selectD')); }
+	this.Deselectd = function (num) { throw (throw_case('Deck.seselectD')); }
+
+	this.selectv = function (num) { throw (throw_case('Deck.selectV')); }
+	this.deselectv = function (num) { throw (throw_case('Deck.deselectV')); }
+	this.selectd = function (num) { throw (throw_case('Deck.selectD')); }
+	this.deselectd = function (num) { throw (throw_case('Deck.seselectD')); }
+
+	this.selectall = function (num) { throw (throw_case('Deck.selectAll')); }
+	this.deselectall = function (num) { throw (throw_case('Deck.deselectAll')); }
+
+	this.SelectAll = function (num) { throw (throw_case('Deck.selectAll')); }
+	this.DeselectAll = function (num) { throw (throw_case('Deck.deselectAll')); }
 }
+
+function throw_case(correct) { return 'Wrong casing.  Correct version of this call: ' + correct + '(...)'; }
